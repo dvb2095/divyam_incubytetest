2a07de019368e86fd96b03b515f1c3ebbe792626
dvb2095 <divyam2095@gmail.com>
2021-03-13 19:41:17 +0530

    Test for all delimiters

--- incubyte_test/TDD_StringTest_TestIncubyte.java
+++ incubyte_test/TDD_StringTest_TestIncubyte.java
@@ -6,44 +6,46 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Assert.*;
 
 public class TDD_StringTest_TestIncubyte {
+	ProdTestIncubyte prod = new ProdTestIncubyte();
 	@Test
 	public void checkempty() {
-		ProdTestIncubyte prod = new ProdTestIncubyte();
 		assertEquals(0, prod.Add(""));
 	}
 	
 	@Test
 	public void checkonenumber() {
-		ProdTestIncubyte prod = new ProdTestIncubyte();
 		assertEquals(1, prod.Add("1"));
 		assertEquals(2, prod.Add("2"));
 	}
 	
 	@Test
 	public void checktwonumbers() {
-		ProdTestIncubyte prod = new ProdTestIncubyte();
 		assertEquals(5, prod.Add("2,3"));
 		assertEquals(3, prod.Add("1,2"));		
 	}
 	
 	@Test
 	public void checkunknownamountofnumbers() {
-		ProdTestIncubyte prod = new ProdTestIncubyte();
 		assertEquals(6, prod.Add("2,2,2"));
 		assertEquals(45, prod.Add("1,2,3,4,5,6,7,8,9"));
 	}
 	
 	@Test
 	public void checkHandleNewLine() {
-		ProdTestIncubyte prod = new ProdTestIncubyte();
 		assertEquals(6, prod.Add("2,2,\n2"));
 		assertEquals(45, prod.Add("1,2,3\n,4,5,\n6,7,8,\n9"));
 	}
 	
 	@Test
 	public void checkdelimiter() {
-		ProdTestIncubyte prod = new ProdTestIncubyte();
 		assertEquals(6, prod.Add("2;,2,\n2"));
 		assertEquals(45, prod.Add("//1,;2,3\n,4,\\5,\n6,7,;8,\n9"));
 	}
+	
+	@Test
+	public void checkdelimiter2() {
+		assertEquals(6, prod.Add("2;,2,\n2"));
+		assertEquals(45, prod.Add("//1,;2,^&3\n,4,\\5,\n6,7,;8,\n9"));
+	}
+
 }
